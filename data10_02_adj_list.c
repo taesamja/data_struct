1be777a0f302da79802e80766f129cc5d1075f54
taesamja <taesamja12345@gmail.com>
2023-06-12 09:06:12 +0900

    그래프_인접리스트_사용

---
+++ data10_02_adj_list.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VERTICES 50
+typedef struct GraphNode
+{
+    int vertex;
+    struct GraphNode* link;
+} GraphNode;
+
+typedef struct GraphType
+{
+    int n; // number of vertex
+    GraphNode* adj_list[MAX_VERTICES];
+} GraphType;
+
+// To initialize a graph
+void init(GraphType* g)
+{
+    int v;
+    g->n = 0;
+    for(v=0; v<MAX_VERTICES; v++)
+        g->adj_list[v] = NULL;
+}
+
+// vertex insertion operation
+void insert_vertex(GraphType* g, int v)
+{
+    if((g->n + 1) > MAX_VERTICES) {
+        fprintf(stderr, "그래프: 정점의 개수 초과");
+    }
+    g->n++;
+}
+
+// edge insertion operation
+// the vertex v is inserted into the adjacency list of u
+void insert_edge(GraphType* g, int u, int v)
+{
+    GraphNode* node;
+    if(u >= g->n || v >= g->n) {
+        fprintf(stderr, "그래프: 정점의 번호 오류");
+    }
+    node = (GraphNode*)malloc(sizeof(GraphNode));
+    node->vertex = v;
+    node->link = g->adj_list[u];
+    g->adj_list[u] = node;
+}
+
+// Output function of adjacency linkedlist
+void print_adj_list(GraphType* g)
+{
+    for(int i=0; i < g->n; i++) 
+    {
+        GraphNode* p = g->adj_list[i];
+        printf("정점 %d의 인접 리스트 ", i);
+        while(p!=NULL) {
+            printf("-> %d ", p->vertex);
+            p = p->link;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    GraphType* g;
+    g = (GraphType *)malloc(sizeof(GraphType));
+
+    init(g);
+
+    for(int i=0; i<4; i++)
+        insert_vertex(g, i);
+    
+    insert_edge(g, 0, 1);
+    insert_edge(g, 0, 2);
+    insert_edge(g, 0, 3);
+    insert_edge(g, 1, 0);
+    insert_edge(g, 1, 2);
+    insert_edge(g, 2, 0);
+    insert_edge(g, 2, 1);
+    insert_edge(g, 2, 3);
+    insert_edge(g, 3, 0);
+    insert_edge(g, 3, 2);
+
+    print_adj_list(g);
+
+    free(g);
+
+    return 0;
+}
